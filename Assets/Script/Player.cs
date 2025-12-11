a0b0d6bd91fa37461df850d250c2c47fe7c5a445
ArmanGoodarziii <armangoodarziii21@gmail.com>
2025-12-11 16:57:09 +0330

    Add camera controller logic

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+    [Header("Movement")]
     private Animator animator;
     public float speed;
     public FixedJoystick myJoyStick;
-     
+    [Header("Camera")]
+    private bool cameraMove = true;
+    public GameObject cameraObject;
+    public Vector3 plusVec;
+    public float speedCamera;
 
     void Start()
     {
@@ -14,6 +19,17 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        if (cameraMove)
+        {
+            cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position , new Vector3(transform.position.x + plusVec.x , cameraObject.transform.position.y + plusVec.y , transform.position.z + plusVec.z) , speedCamera * Time.deltaTime);
+            cameraObject.transform.LookAt(transform.position);
+        }
+        else
+        {
+            cameraObject.transform.LookAt(transform.position);
+        }
+
         if(myJoyStick.Horizontal != 0 || myJoyStick.Vertical != 0)
         {
             transform.Translate(new Vector3(0 , 0 , speed * Time.deltaTime));
@@ -36,5 +52,18 @@ public class Player : MonoBehaviour
             animator.SetBool("run", false);
         }
     }
-    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "camera")
+        {
+            cameraMove = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "camera")
+        {
+            cameraMove = false;
+        }
+    }
 }
