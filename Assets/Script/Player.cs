13aa8ea81cc9a2f3c94c218869c08e063c4e7faf
ArmanGoodarziii <armangoodarziii21@gmail.com>
2025-12-11 21:48:49 +0330

    Update the game play and Import or update assets.
    Update Game UI. Create attack buttons, Profile image and health bar.
    Impelemente the Player attack logic and  Impelemente the logic of effects for attacks in Player.cs .
    Add Events to player animations for attacks.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
-{   
+{
+    public GameObject[] effects;
+
+    [Header("Button's images")]
+    public Image StrongAttack_1_img;
+    public Image StrongAttack_2_img;
+    public Image StrongAttack_3_img;
+    // Button's field time 
+    private float StrongAttack_1_time;
+    private float StrongAttack_2_time;
+    private float StrongAttack_3_time;
     [Header("Movement")]
     private Animator animator;
     public float speed;
@@ -11,6 +22,7 @@ public class Player : MonoBehaviour
     public GameObject cameraObject;
     public Vector3 plusVec;
     public float speedCamera;
+    private float waitAnimation;
 
     void Start()
     {
@@ -30,26 +42,77 @@ public class Player : MonoBehaviour
             cameraObject.transform.LookAt(transform.position);
         }
 
-        if(myJoyStick.Horizontal != 0 || myJoyStick.Vertical != 0)
+        if(waitAnimation <= 0)
         {
-            transform.Translate(new Vector3(0 , 0 , speed * Time.deltaTime));
+            if(myJoyStick.Horizontal != 0 || myJoyStick.Vertical != 0)
+            {
+                transform.Translate(new Vector3(0 , 0 , speed * Time.deltaTime));
+            }
         }
     }
     void Update()
     {
-        float moveH = myJoyStick.Horizontal;
-        float moveV = myJoyStick.Vertical;
-
-        Vector3 direction =new Vector3(moveH , 0 , moveV);
-
-        if(myJoyStick.Horizontal != 0 || myJoyStick.Vertical != 0)
+        if(StrongAttack_1_time > 0)
         {
-            transform.LookAt(transform.position - direction);
-            animator.SetBool("run", true);
+            StrongAttack_1_img.enabled = true;
+            StrongAttack_1_time -= 1 * Time.deltaTime;
+            StrongAttack_1_img.fillAmount = StrongAttack_1_time / 9;
         }
-        else if(myJoyStick.Horizontal == 0 || myJoyStick.Vertical == 0)
+        else if(StrongAttack_1_time <= 0)
         {
-            animator.SetBool("run", false);
+            StrongAttack_1_img.enabled = false;
+            StrongAttack_1_time = 0;
+        }
+
+        if(StrongAttack_2_time > 0)
+        {
+            StrongAttack_2_img.enabled = true;
+            StrongAttack_2_time -= 1 * Time.deltaTime;
+            StrongAttack_2_img.fillAmount = StrongAttack_2_time / 8;
+        }
+        else if(StrongAttack_2_time <= 0)
+        {
+            StrongAttack_2_img.enabled = false;
+            StrongAttack_2_time = 0;
+        }
+
+        if(StrongAttack_3_time > 0)
+        {
+            StrongAttack_3_img.enabled = true;
+            StrongAttack_3_time -= 1 * Time.deltaTime;
+            StrongAttack_3_img.fillAmount = StrongAttack_3_time / 15;
+        }
+        else if(StrongAttack_1_time <= 0)
+        {
+            StrongAttack_3_img.enabled = false;
+            StrongAttack_3_time = 0;
+        }
+
+        if(waitAnimation <= 0)
+        {
+            float moveH = myJoyStick.Horizontal;
+            float moveV = myJoyStick.Vertical;
+
+            Vector3 direction =new Vector3(moveH , 0 , moveV);
+
+            if(myJoyStick.Horizontal != 0 || myJoyStick.Vertical != 0)
+            {
+                transform.LookAt(transform.position - direction);
+                animator.SetBool("run", true);
+            }
+            else if(myJoyStick.Horizontal == 0 || myJoyStick.Vertical == 0)
+            {
+                animator.SetBool("run", false);
+            }
+        }
+
+        if(waitAnimation > 0)
+        {
+            waitAnimation -= 1 * Time.deltaTime;
+        }
+        else if(waitAnimation <= 0)
+        {
+            waitAnimation = 0;
         }
     }
     void OnTriggerEnter(Collider other)
@@ -65,5 +128,63 @@ public class Player : MonoBehaviour
         {
             cameraMove = false;
         }
+    }
+
+    public void Attack()
+    {
+        if(waitAnimation <= 0)
+        {
+            animator.Play("attack");
+            waitAnimation = 1.2f;
+        }
+    }
+    public void StrongAttack_1()
+    {
+        if(waitAnimation <= 0)
+        {
+            animator.Play("great-attack-1");
+            waitAnimation = 5f;
+            StrongAttack_1_time = 9;
+        }
+    }
+    public void StrongAttack_2()
+    {
+        if(waitAnimation <= 0)
+        {
+            animator.Play("great-attack-2");
+            waitAnimation = 2f;
+            StrongAttack_2_time = 8;
+        }
+    }
+    public void StrongAttack_3()
+    {
+        if(waitAnimation <= 0)
+        {
+            animator.Play("great-attack-3");
+            waitAnimation = 2f;
+            StrongAttack_3_time = 15;
+        }
+    }
+    
+    public void Event_Attack()
+    {
+        Instantiate(effects[3], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
+        Instantiate(effects[4], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
+    }
+    public void Event_Charge()
+    {
+        Instantiate(effects[0], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
+    }
+    public void Event_StrongAttack_1()
+    {
+        Instantiate(effects[1], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
+    }
+    public void Event_StrongAttack_2()
+    {
+        Instantiate(effects[2], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
+    }
+    public void Event_StrongAttack_3()
+    {
+        Instantiate(effects[5], new Vector3(transform.position.x, 1.1f, transform.position.z), Quaternion.identity);
     }
 }
